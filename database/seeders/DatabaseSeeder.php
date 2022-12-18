d867cb803c6615b2a0df15d26d7d5a6c90837a3b
Lilkizarmie <jefferyadolor18@gmail.com>
2022-12-18 17:42:08 +0100

    admin created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(Admin::class)
+        $this->call(Admin::class);
         
     }
 }
